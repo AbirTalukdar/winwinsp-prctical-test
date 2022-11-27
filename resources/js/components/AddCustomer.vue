210189f20ba7044c5afa370cf3815db031f17243
AbirTalukdar <imtiaz.nafes@gmail.com>
2022-11-27 07:14:24 -0800

    adding javascript

--- resources/js/components/AddCustomer.vue
+++ resources/js/components/AddCustomer.vue
@@ -4,34 +4,36 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">WWSP</div>
-                    <div class="card-body">
-                        <table class="table">
-                        <thead>
-                            <tr>
-                            <th scope="col">Code</th>
-                            <th scope="col">Full Name</th>
-                            <th scope="col">Age</th>
-                            <th scope="col">Location</th>
-                            <th scope="col"><button type="button" class="btn btn-sm btn-primary mt-3" @click.prevent="add()">+</button></th>
-
-                            </tr>
-                        </thead>
-                            <tbody v-for="(item, i) in addData" :key="i">
+                    <form @submit.prevent="register">
+                        <div class="card-body">
+                            <table class="table mt-2">
+                            <thead>
                                 <tr>
-                                <td scope="row"><input v-model="item.code"/></td>
-                                <td><input v-model="item.full_name"/></td>
-                                <td><input v-model="item.age"/></td>
-                                <td><input v-model="item.location"/></td>
-                                <td><button v-if="i > 0" type="button" class="btn btn-sm btn-danger mt-0" @click="remove(i)">-</button></td>
+                                <th scope="col">Code</th>
+                                <th scope="col">Full Name</th>
+                                <th scope="col">Age</th>
+                                <th scope="col">Location</th>
+                                <th scope="col"><button type="button" class="btn btn-sm btn-primary mt-3" @click.prevent="add()">+</button></th>
+
                                 </tr>
-                            </tbody>
-                            <div class="row">
-                                <div class="col text-right">
-                                    <button type="submit" variant="primary" class="btn btn-sm btn-primary mr-2">Save</button>
+                            </thead>
+                                <tbody v-for="(item, i) in addData" :key="i">
+                                    <tr>
+                                    <td scope="row"><input v-model="item.code"/></td>
+                                    <td><input v-model="item.full_name"/></td>
+                                    <td><input v-model="item.age"/></td>
+                                    <td><input v-model="item.location"/></td>
+                                    <td><button v-if="i > 0" type="button" class="btn btn-sm btn-danger mt-0" @click="remove(i)">-</button></td>
+                                    </tr>
+                                </tbody>
+                                <div class="row">
+                                    <div class="justify-content-end">
+                                        <button type="submit" class="btn btn-primary mr-2">Save</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </table>
-                    </div>
+                            </table>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -53,6 +55,10 @@
         }
        },
        methods:{
+        async register () {
+            console.log(this.addData);
+            await RestApi.postData(disabilitySchoolServiceBaseUrl, transportationManagementStoreApi, this.data)
+        },
         add () {
             this.addData.push({
                     code: '',
